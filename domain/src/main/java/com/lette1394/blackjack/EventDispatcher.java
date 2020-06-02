668494b4e6266dd4a4fe06a19d804362125d52f7
lette1394 <lette1394@gmail.com>
2020-06-03 00:19:50 +0900

    [Red] Add EventDispatcherTest

---
+++ domain/src/main/java/com/lette1394/blackjack/EventDispatcher.java
@@ -0,0 +1,11 @@
+package com.lette1394.blackjack;
+
+public class EventDispatcher<T> {
+    public void add(final T listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void dispatch() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ domain/src/test/java/com/lette1394/blackjack/EventDispatcherTest.java
@@ -0,0 +1,27 @@
+package com.lette1394.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+
+import com.lette1394.blackjack.util.BaseTest;
+
+class EventDispatcherTest extends BaseTest {
+    interface SampleListener {
+        void call();
+    }
+
+    private final EventDispatcher<SampleListener> dispatcher = new EventDispatcher<>();
+    @Mock
+    private SampleListener sampleListener;
+
+    @Test
+    void eventDispatcherCallSingleListeners() {
+        context.checking(new Expectations() {{
+            oneOf(sampleListener).call();
+        }});
+
+        dispatcher.add(sampleListener);
+        dispatcher.dispatch();
+    }
+}
