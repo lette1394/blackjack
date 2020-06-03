63f51327352df2066200d73013194ad2c2f97505
lette1394 <lette1394@gmail.com>
2020-06-03 23:08:14 +0900

    [Refactoring] Add ConsolePlayerCommander

---
+++ domain/src/main/java/com/lette1394/blackjack/ConsolePlayerCommander.java
@@ -0,0 +1,26 @@
+package com.lette1394.blackjack;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ConsolePlayerCommander implements PlayerCommandListener {
+
+    @Override
+    public void join() {
+    }
+
+    @Override
+    public void hit() {
+
+    }
+
+    @Override
+    public void stay() {
+
+    }
+
+    @Override
+    public void cannotHandle(final String playerInput) {
+
+    }
+}
--- domain/src/main/java/com/lette1394/blackjack/ui/ConsolePlayerInputGameOutput.java
+++ domain/src/main/java/com/lette1394/blackjack/ui/ConsolePlayerInputGameOutput.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConsolePlayerInputGameOutput implements PlayerInputGameOutput {
-
     private final Scanner in;
     private final PrintStream out;
 
