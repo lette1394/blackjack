7247b99811963e6aa25a17b4a33486bde22f46d6
lette1394 <lette1394@gmail.com>
2020-06-04 01:20:18 +0900

    [Refactoring] Remove comments

--- domain/src/main/java/com/lette1394/blackjack/BlackjackGame.java
+++ domain/src/main/java/com/lette1394/blackjack/BlackjackGame.java
@@ -6,9 +6,6 @@ public class BlackjackGame implements BlackjackPlayerCommandListener {
     private Trumps trumpsForDealer;
     private Trumps trumpsForPlayer = new Trumps();
 
-    // TODO: score도 마찬가지. 확실히 Trump 클래스에 들어가는 건 아닌 거 같고, 테스트를 조금 더 추가해서 생각해보자.
-    //  아마도 1:1이 아니라 1:N으로 게임할때, 그러니까 딜러는 한명이고 여러 다른 사람들이 게임할때 테스트를 추가하면 더 잘 드러날듯.
-
     private final CardProvider cardProvider;
     private final EventAnnouncer<BlackjackGameEventListener> game = new EventAnnouncer<>(BlackjackGameEventListener.class);
 
