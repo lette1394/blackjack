b46519004c93a80efea0f72340aea7fb7a3f9add
lette1394 <lette1394@gmail.com>
2020-06-04 23:31:59 +0900

    [Green] Add TrumpFactory test

--- domain/src/test/java/com/lette1394/blackjack/TrumpFactoryTest.java
+++ domain/src/test/java/com/lette1394/blackjack/TrumpFactoryTest.java
@@ -9,7 +9,30 @@ class TrumpFactoryTest {
     @Test
     void trumpFactoryMakesNumericTrump() {
         final Trump trump = TrumpFactory.trump("♦️", "5");
-
         assertThat(trump, is(new Trump(Trump.Suit.DIAMOND, Trump.Value.FIVE)));
+    }
+
+    @Test
+    void trumpFactoryMakesAceTrump() {
+        final Trump trump = TrumpFactory.trump("♦️", "Ace");
+        assertThat(trump, is(new Trump(Trump.Suit.DIAMOND, Trump.Value.ACE)));
+    }
+
+    @Test
+    void trumpFactoryMakesJackTrump() {
+        final Trump trump = TrumpFactory.trump("♦️", "Jack");
+        assertThat(trump, is(new Trump(Trump.Suit.DIAMOND, Trump.Value.JACK)));
+    }
+
+    @Test
+    void trumpFactoryMakesQueenTrump() {
+        final Trump trump = TrumpFactory.trump("♦️", "Queen");
+        assertThat(trump, is(new Trump(Trump.Suit.DIAMOND, Trump.Value.QUEEN)));
+    }
+
+    @Test
+    void trumpFactoryMakesKingTrump() {
+        final Trump trump = TrumpFactory.trump("♦️", "King");
+        assertThat(trump, is(new Trump(Trump.Suit.DIAMOND, Trump.Value.KING)));
     }
 }
