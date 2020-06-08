553a85340243f15110827bf1002e1aa1a5a0ae42
Jae Eun Na <jae-eun.na@linecorp.com>
2020-06-08 17:29:12 +0900

    [Green] Pass BlackjackGameSnapshotTest#newGame

--- domain/src/main/java/com/lette1394/blackjack/domain/BlackjackGameSnapshot.java
+++ domain/src/main/java/com/lette1394/blackjack/domain/BlackjackGameSnapshot.java
@@ -1,23 +1,28 @@
 package com.lette1394.blackjack.domain;
 
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
 public class BlackjackGameSnapshot {
+    private final State state;
+
+    private BlackjackGameSnapshot(State state) {
+        this.state = state;
+    }
+
     public static BlackjackGameSnapshot newGame() {
-        throw new UnsupportedOperationException();
+        return waiting();
     }
 
-    public static Object waiting() {
-        throw new UnsupportedOperationException();
+    public static BlackjackGameSnapshot waiting() {
+        return new BlackjackGameSnapshot(State.WAITING);
     }
 
-    private enum BlackjackGameState {
-        READY,
+
+
+    private enum State {
+        WAITING,
         RUNNING,
-        FINISHED,
-        ;
-
-        public static BlackjackGameState newState() {
-
-            return null;
-        }
+        FINISHED
     }
 }
